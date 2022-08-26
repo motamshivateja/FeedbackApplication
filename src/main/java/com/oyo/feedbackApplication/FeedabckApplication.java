419bff48881bff6230875d125adf0fdc196e6aeb
MOTAM SHIVA TEJA <svd@MOTAMs-MacBook-Pro.local>
2022-08-26 11:22:21 +0530

    First Commit

---
+++ src/main/java/com/oyo/feedbackApplication/FeedabckApplication.java
@@ -0,0 +1,13 @@
+package com.oyo.feedbackApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedabckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedabckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oyo/feedbackApplication/FeedabckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyo.feedbackApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedabckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
